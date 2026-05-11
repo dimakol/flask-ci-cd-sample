d20fdf2ab7d7af821c6e9dc3f9a2fd2726a82aa7
dimakol <dimakol@gmail.com>
2026-05-11 15:19:31 +0300

    ci(ci.yaml): add lint with flake8

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return { "message": "CI/CD Demo" }
+    return {"message": "CI/CD Demo"}
+
 
 @app.route("/health")
 def health():
-    return { "status": "ok" }
+    return {"status": "ok"}
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host='0.0.0.0')
